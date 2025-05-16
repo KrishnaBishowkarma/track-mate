d1f9169fb1257d213fd117bb56ebc1be4d62314e
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-05-16 19:22:38 +0545

    Initial commit - Spring Boot setup

---
+++ src/main/java/np/com/krishnabk/trackmate/TrackMateApplication.java
@@ -0,0 +1,13 @@
+package np.com.krishnabk.trackmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/np/com/krishnabk/trackmate/TrackMateApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.krishnabk.trackmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
