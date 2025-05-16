7e4d738cc60779b455d17f29f7ceb5dac6812f86
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-05-16 19:49:30 +0545

    feat: add devtools dependency and test endpoints to verify hot reload

--- src/main/java/np/com/krishnabk/trackmate/controller/HelloRestController.java
+++ src/main/java/np/com/krishnabk/trackmate/controller/HelloRestController.java
@@ -11,4 +11,16 @@ public class HelloRestController {
     public String sayHello(){
         return "Hey there, How are you?";
     }
+
+    // Expose "/hi" endpoint to check if spring-boot-devtools woking or not
+    @GetMapping("/hi")
+    public String sayHi(){
+        return "Hi there, How are you?";
+    }
+
+    // Expose "/lucky" endpoint to reverify the spring-boot-devtools function
+    @GetMapping("/lucky")
+    public String sayLucky(){
+        return "Today is your lucky day!";
+    }
 }
